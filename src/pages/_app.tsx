f5e95197427afc407e57cad35309e74aef579bc0
Lucas Bueno Cesario <lbc92@hotmail.com>
2021-04-20 18:40:12 -0300

    Empty player component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import "../styles/global.scss";
 
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+
+import styles from "../styles/app.module.scss";
+
+
+//componentes Player e Header estão em todas as páginas,
+//logo podem estar aqui em _app.tsx
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header /> 
+        <Component {...pageProps} />
+      </main>   
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
